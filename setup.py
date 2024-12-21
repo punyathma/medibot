f673d7931ffd2e2c1d3c3255ad93517564e72290
VISHWAS CHANDRA C <121396465+punyathma@users.noreply.github.com>
2024-12-21 14:21:26 +0530

    folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project' ,
+    version = '0.0.0' ,
+    author = 'Vishwas' , 
+    author_email = 'vishwaschandra1905@gmail.com' , 
+    packages = find_packages() ,
+    install_requires = []
+)
